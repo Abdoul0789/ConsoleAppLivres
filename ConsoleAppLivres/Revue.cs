2c9c196d02bc399d4150796ec473cc4b3d6f1b6b
abdoul <300139695@monboreal.ca>
2024-11-16 18:26:26 -0500

    ajout de la classe enfant Revue

--- ConsoleAppLivres/Revue.cs
+++ ConsoleAppLivres/Revue.cs
@@ -8,16 +8,17 @@ namespace ConsoleAppLivres
 {
     internal class Revue:Livre
     {
-        public int Numero { get; set; }
-        public int Annee { get; set; }
+        public int  Numero { get; set; }
+        public int  Annee { get; set; }
 
         public Revue(int numero, int annee, string titre, string auteur, int nbrsPages) : base(titre, auteur,nbrsPages)
         { 
-          Numero=numero; 
-          Annee=annee;   
+           Numero=numero; 
+           Annee=annee;   
         }
         public override void afficherDetails()
         {
+            
             Console.WriteLine($"le Titre du livre est: {Titre}, l'Auteur est : {Auteur}, NbrsPages est de : {NbrsPages},le numero est:{Numero},l'annee est :{Annee}");
         }
 
