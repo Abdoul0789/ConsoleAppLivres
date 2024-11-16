0a080dce7af7e8417bbf5c79b3162d0318dc206c
abdoul <300139695@monboreal.ca>
2024-11-16 18:24:26 -0500

    Ajout la classe enfant  Roman

--- ConsoleAppLivres/Roman.cs
+++ ConsoleAppLivres/Roman.cs
@@ -9,16 +9,20 @@ namespace ConsoleAppLivres
     internal class Roman:Livre
 
     {
-        public string Genre { get; set; }
+        public string  Genre { get; set; }
 
         public Roman(string titre, string auteur, int nbrsPages,string genre) : base(titre, auteur, nbrsPages)
         {
+               
             Genre= genre;
             
         }
         public override void afficherDetails()
         {
             Console.WriteLine($"le Titre du livre est: {Titre}, l'Auteur est : {Auteur}, NbrsPages est de : {NbrsPages},le genre est:{Genre}");
+
+
+
         }
     }
 }
